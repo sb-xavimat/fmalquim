67b1a21430b6e8204eef3c92a01dfae125dddc00
xavi-mat <1102677+xavi-mat@users.noreply.github.com>
2025-02-28 07:37:58 +0100

    `getMetalOxidesPRE_FN` done

--- js/constants.js
+++ js/constants.js
@@ -1,14 +1,19 @@
 const COLOR = {
     red: "#f00",
     1: "#f00",
+
     orange: "#f90",
     2: "#f90",
-    green: "#0a0",
+
+    green: "#080",
     3: "#080",
+
     lightGreen: "#0b0",
-    4: "#0f0",
+    4: "#0b0",
+
     lilac: "#a0f",
     5: "#a0f",
+
     grey: "#888",
     6: "#888",
 };
